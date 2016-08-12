ee8a097a55bbb07412b9e86440b4cbfbaf4698c6
wombat-kun <wombatukun@gmail.com>
2016-08-12 08:07:37 +0700

    rest service uri

--- src/main/resources/static/js/service/todo_service.js
+++ src/main/resources/static/js/service/todo_service.js
@@ -2,7 +2,7 @@
 
 angular.module('myApp').factory('TodoService', ['$http', '$q', function($http, $q){
 
-    var REST_SERVICE_URI = 'http://localhost:8080/rest/';
+    var REST_SERVICE_URI = '/rest/';
 
     var factory = {
         fetchAllRecords: fetchAllRecords,
